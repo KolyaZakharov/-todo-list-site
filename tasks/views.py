73869cebfc330b3fa1bf44b3763661377c47169f
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-02-21 21:00:05 +0200

    added task_form.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from tasks.models import Task, Tag
@@ -15,3 +16,5 @@ class TagsListView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
