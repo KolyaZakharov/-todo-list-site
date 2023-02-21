ded35bd6969b44892ff3e9a479a6c7f9531c002e
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-02-21 20:59:15 +0200

    installed crispy forms added it to requirements.txt and settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "tasks",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,11 @@ TEMPLATES = [
         },
     },
 ]
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'todo_list.wsgi.application'
 
