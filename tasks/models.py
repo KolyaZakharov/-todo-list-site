25aa7fc73ae2a645c5de2b32c6db428feedef927
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-02-25 18:20:06 +0200

    modified todo_list_data.json

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -23,3 +24,5 @@ class Task(models.Model):
 
     def __str__(self):
         return self.content
+
+
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
